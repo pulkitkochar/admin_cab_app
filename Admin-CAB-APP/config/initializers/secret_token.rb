77cb814fb04759862be3b0566c95d7749db77aef
Pulkit <pulkit@Event9.(none)>
2013-01-23 10:29:44 +0530

    first commit

---
+++ Admin-CAB-APP/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdminCabApp::Application.config.secret_token = '0ec21f490dd5fe2c82cc4e049b7b5bc764b8248038e0b1c9fed29c4a96317d9732af53381c1f116d712815df243e90cffe598c1c381f4c8c2c584c2eacf38d02'
